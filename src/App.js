31f800a704f268fdb9326153a9d9b097af53eb71
Francisco Castillo <cas21562@uvg.edu.gt>
2023-05-20 17:14:06 -0600

    Function Calling Implemented but not functional

--- src/App.js
+++ src/App.js
@@ -5,29 +5,55 @@ const calculator = {
 };
 
 const App = () => {
-    const [displayValue, setDisplayValue] = useState('0');
+    const [currentValue, setCurrentValue] = useState(0);
+    const [previousValue, setPreviousValue] = useState(null);
+    const [operation, setOperation] = useState(null);
 
-    const handleNumberClick = (event, number) => {
+    const handleNumberPress = (event, number) => {
         if (!number) {
             number = event.target.textContent;
         }
-        setDisplayValue((prevValue) => {
-            return prevValue === '0' ? number : prevValue + number;
+        setCurrentValue((prevValue) => {
+            return prevValue === 0 ? number : prevValue + number;
         });
     }
 
-    const handleDecimalClick = (event) => {
-        setDisplayValue((prevValue) => {
+    const handleDecimalPress = () => {
+        setCurrentValue((prevValue) => {
             // Append the pressed key to the current display value
             return prevValue.includes('.') ? prevValue : prevValue + '.';
         });
     }
 
     const handleBackspacePress = () => {
-        setDisplayValue((prevValue) => {
+        setCurrentValue((prevValue) => {
             // Remove the last character from the current display value
             return prevValue.length === 1 ? '0' : prevValue.slice(0, -1);
         });
+    }
+
+    const handleOperationPress = (event, operation) => {
+        if(!operation) {
+            operation = event.target.textContent;
+        }
+        setPreviousValue(currentValue);
+        setCurrentValue(0);
+    }
+
+    const handleEqualsPress = () => {
+    };
+
+    const handleClear = () => {
+        setCurrentValue(0);
+        setPreviousValue(null);
+    }
+
+    const handleSignPress = () => {
+
+    }
+
+    const handlePercentagePress = () => {
+
     }
 
     useEffect(() => {
@@ -35,25 +61,18 @@ const App = () => {
             const key = event.key;
 
             if (/^\d$/.test(key)) {
-                handleNumberClick({target: {textContent: key}})
+                handleNumberPress({target: {textContent: key}})
             } else if (key === '.') {
-                handleDecimalClick();
+                handleDecimalPress();
             } else if (key === 'Backspace') {
                 handleBackspacePress();
             } else if (key === 'Escape') {
-                setDisplayValue('0');
+                handleClear();
+            } else if (key === '+' || key === '-' || key === '*' || key === '/') {
+                handleOperationPress({target: {textContent: key}});
+            } else if (key === 'Enter') {
+                handleEqualsPress();
             }
-            // } else if (key === '+' || key === '-' || key === '*' || key === '/') {
-            //     setDisplayValue((prevValue) => {
-            //         // Append the pressed key to the current display value
-            //         return prevValue + key;
-            //     });
-            // } else if (key === 'Enter') {
-            //     setDisplayValue((prevValue) => {
-            //         // Append the pressed key to the current display value
-            //         return eval(prevValue);
-            //     });
-            // }
         };
 
         document.addEventListener('keydown', handleKeyDown);
@@ -66,63 +85,63 @@ const App = () => {
     return (
         <div className="container">
             <div className="calculator">
-                <div className="display">{displayValue}</div>
+                <div className="display">{currentValue}</div>
                 <div className="buttons">
-                    <button className="button-function" id="clear">
+                    <button className="button-function" id="clear" onClick={handleClear}>
                         C
                     </button>
-                    <button className="button-function" id="sign">
+                    <button className="button-function" id="sign" onClick={handleSignPress}>
                         +/-
                     </button>
-                    <button className="button-function" id="percentage">
+                    <button className="button-function" id="percentage" onClick={handlePercentagePress}>
                         %
                     </button>
-                    <button className="button-operation" id="division">
+                    <button className="button-operation" id="division" onClick={(event) => handleOperationPress(event, '/')}>
                         ÷
                     </button>
-                    <button className="button-number" id="seven" onClick={(event) => handleNumberClick(event, '7')}>
+                    <button className="button-number" id="seven" onClick={(event) => handleNumberPress(event, '7')}>
                         7
                     </button>
-                    <button className="button-number" id="eight" onClick={(event) => handleNumberClick(event, '8')}>
+                    <button className="button-number" id="eight" onClick={(event) => handleNumberPress(event, '8')}>
                         8
                     </button>
-                    <button className="button-number" id="nine" onClick={(event) => handleNumberClick(event, '9')}>
+                    <button className="button-number" id="nine" onClick={(event) => handleNumberPress(event, '9')}>
                         9
                     </button>
-                    <button className="button-operation" id="multiplication">
+                    <button className="button-operation" id="multiplication" onClick={(event) => handleOperationPress(event, '*')}>
                         x
                     </button>
-                    <button className="button-number" id="four" onClick={(event) => handleNumberClick(event, '4')}>
+                    <button className="button-number" id="four" onClick={(event) => handleNumberPress(event, '4')}>
                         4
                     </button>
-                    <button className="button-number" id="five" onClick={(event) => handleNumberClick(event, '5')}>
+                    <button className="button-number" id="five" onClick={(event) => handleNumberPress(event, '5')}>
                         5
                     </button>
-                    <button className="button-number" id="six" onClick={(event) => handleNumberClick(event, '6')}>
+                    <button className="button-number" id="six" onClick={(event) => handleNumberPress(event, '6')}>
                         6
                     </button>
-                    <button className="button-operation" id="subtraction">
+                    <button className="button-operation" id="subtraction" onClick={(event) => handleOperationPress(event, '-')}>
                         -
                     </button>
-                    <button className="button-number" id="one" onClick={(event) => handleNumberClick(event, '1')}>
+                    <button className="button-number" id="one" onClick={(event) => handleNumberPress(event, '1')}>
                         1
                     </button>
-                    <button className="button-number" id="two" onClick={(event) => handleNumberClick(event, '2')}>
+                    <button className="button-number" id="two" onClick={(event) => handleNumberPress(event, '2')}>
                         2
                     </button>
-                    <button className="button-number" id="three" onClick={(event) => handleNumberClick(event, '3')}>
+                    <button className="button-number" id="three" onClick={(event) => handleNumberPress(event, '3')}>
                         3
                     </button>
-                    <button className="button-operation" id="addition">
+                    <button className="button-operation" id="addition" onClick={(event) => handleOperationPress(event, '+')}>
                         +
                     </button>
-                    <button className="button-number" id="zero">
+                    <button className="button-number" id="zero" onClick={(event) => handleNumberPress(event, '0')}>
                         0
                     </button>
-                    <button className="button-number" id="decimal" onClick={handleDecimalClick}>
+                    <button className="button-number" id="decimal" onClick={handleDecimalPress}>
                         .
                     </button>
-                    <button className="button-equal" id="equals">
+                    <button className="button-equal" id="equals" onClick={handleEqualsPress}>
                         =
                     </button>
                 </div>
